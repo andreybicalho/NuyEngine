1dc73688b8c3acca14185fdb2ecd5bcdb4c8cd5f
Andrey <andreybicalho@gmail.com>
2018-07-26 20:50:55 -0300

    light follwing mouse
    playing a bit with shaders

--- NuyEngine/NuyEngine-core/Source/Main.cpp
+++ NuyEngine/NuyEngine-core/Source/Main.cpp
@@ -4,6 +4,7 @@
 #include "Maths/Maths.h"
 #include "Graphics/Shader.h"
 
+
 int main()
 {
 	nuy::graphics::Window window("Nuy Engine", 960, 540);
@@ -30,16 +31,23 @@ int main()
 	nuy::graphics::Shader shader("Source/Resources/vertexShader.shader", "Source/Resources/fragmentShader.shader");
 	shader.Enable();
 	shader.SetUniformMat4("pr_matrix", ortho);
-	shader.SetUniformMat4("ml_matrix", nuy::maths::Matrix4::Translate(nuy::maths::Vector3(0.6, 0, 0)));
-	shader.SetUniformMat4("ml_matrix", nuy::maths::Matrix4::Rotate(45.0f, nuy::maths::Vector3(0, 0, 1)));
+	//shader.SetUniformMat4("ml_matrix", nuy::maths::Matrix4::Translate(nuy::maths::Vector3(0.6, 0, 0)));
+	//shader.SetUniformMat4("ml_matrix", nuy::maths::Matrix4::Rotate(45.0f, nuy::maths::Vector3(0, 0, 1)));
 
-	shader.SetUniform2f("light_pos", nuy::maths::Vector2(0.1f, 0.4f));
-	shader.SetUniform4f("colour", nuy::maths::Vector4(0.2f, 0.1f, 0.0f, 1.0f));
+	shader.SetUniform2f("in_light_pos", nuy::maths::Vector2(0.0f, 0.0f));
+	shader.SetUniform4f("in_color", nuy::maths::Vector4(0.2f, 0.1f, 0.0f, 1.0f));
 
 	while (!window.IsClosed())
 	{
 		window.Clear();
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		double x, y;
+		window.GetMousePosition(x, y);
+		//std::cout << x << ", " << y << std::endl;
+		x = (x / 960.0f - 0.5f); 
+		y = (0.5f - y / 540.0f);
+		std::cout << x << ", " << y << std::endl;
+		shader.SetUniform2f("in_light_pos", nuy::maths::Vector2((float)x, (float)y));
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		window.Update();
 	}
 
--- NuyEngine/NuyEngine-core/Source/Resources/fragmentShader.shader
+++ NuyEngine/NuyEngine-core/Source/Resources/fragmentShader.shader
@@ -2,13 +2,13 @@
 
 layout(location = 0) out vec4 color;
 
-uniform vec4 colour;
-uniform vec2 light_pos;
+uniform vec4 in_color;
+uniform vec2 in_light_pos;
 
 in vec4 pos;
 
 void main()
 {
-	float intensity = 1.0 / length(pos.xy - light_pos);
-	color = colour * intensity;
+	float intensity = 1.0 / length(pos.xy - in_light_pos);
+	color = in_color * intensity;
 }
